9a35d5a1479e08a6f5616e3ec21b93ebf1633d76
YoonHwikyung <yoonhwikyung@naver.com>
2023-01-17 23:20:50 +0900

    feat : 도메인 분석 설계, 엔티티 클래스 개발 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -3,13 +3,23 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String streets;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String streets, String zipcode) {
+        this.city = city;
+        this.streets = streets;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders")
@@ -14,7 +19,36 @@ public class Order {
     @Column(name ="order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // 매핑 : fk name이 member_id가 된다 // * 연관관계의 주인
     private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) //
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; //주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; //주문상태 [ORDER, CANCLE]
+
+    //==연관관계 메서드==//
+    public void SetMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
